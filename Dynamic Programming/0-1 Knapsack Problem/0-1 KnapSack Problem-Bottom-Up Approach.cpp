da9c4a4cf5c54e7bd42eb57e8c9ac00ea261950c
Shivam Gupta <66057250+shivamgt@users.noreply.github.com>
2021-05-08 10:25:07 +0530

    Update 0-1 KnapSack Problem-Bottom-Up Approach.cpp

--- Dynamic Programming/0-1 Knapsack Problem/0-1 KnapSack Problem-Bottom-Up Approach.cpp
+++ Dynamic Programming/0-1 Knapsack Problem/0-1 KnapSack Problem-Bottom-Up Approach.cpp
@@ -1,17 +1,17 @@
 	int knapSack(int W, int wt[], int val[], int n) 
     { 
        int dp[n+1][W+1];
-       for(int i=0;i<=n;i++)       //Intialize matrix with 0 for 0th row and column
+       for(int i=0;i<n+1;i++)       //Intialize matrix with 0 for 0th row and column
        {
-           for(int j=0;j<=W+1;j++)
+           for(int j=0;j<W+1;j++)
            {
                if(i==0 || j==0)
                    dp[i][j]=0;
            }
        }
-       for(int i=1;i<=n;i++)      //Calulating the maximum profit
+       for(int i=1;i<n+1;i++)      //Calulating the maximum profit
        {
-           for(int j=1;j<=W;j++)
+           for(int j=1;j<W+1;j++)
            {
                 if(wt[i-1]<=j)	  //Check if weight is less than weight of bag
                 
