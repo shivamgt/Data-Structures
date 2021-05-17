30a643393d7870008dcc16b146ba69ec2aaefd9b
shivamgt <66057250+shivamgt@users.noreply.github.com>
2021-05-17 11:37:51 +0530

    Create Longest Palindromic subsequence.cpp

---
+++ Dynamic Programming/LCS/Longest Palindromic subsequence.cpp
@@ -0,0 +1,33 @@
+	int lcs(string s1,string s2,int m,int n)
+    {
+        int dp[m+1][n+1];
+        
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+            }
+        }
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
+    }
+    
+    int longestPalindromeSubseq(string s1) 
+    {
+        string s2 = s1; 
+        reverse(s2.begin(), s2.end());
+        
+        int m = s1.size();
+        int n = s2.size();
+        
+        return lcs(s1,s2,m,n);
+    }
+
