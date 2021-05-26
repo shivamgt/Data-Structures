b27a4e7b622aeff303eb1fa07bfa89ab4c0c2388
shivamgt <66057250+shivamgt@users.noreply.github.com>
2021-05-26 21:09:47 +0530

    Updated

---
+++ Dynamic Programming/LCS/Minimum number of insertion to make palindrome.cpp
@@ -0,0 +1,32 @@
+int lcs(string s1, string s2, int m, int n)
+    {
+        int dp[m+1][n+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+            }
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j]= 1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
+    }
+  
+    int minimumNumberOfInsertion(string s1)
+    { 
+        string s2=s1;
+        reverse(s2.begin(),s2.end());
+        int m=s1.size();
+        int n=s2.size();
+        
+        int res=m-lcs(s1,s2,m,n);
+        return res;
+    } 
+
