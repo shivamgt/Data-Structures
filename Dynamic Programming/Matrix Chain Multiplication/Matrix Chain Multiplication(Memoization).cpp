db77543ff8e3a95decf491580949d374ba626ea9
shivamgt <66057250+shivamgt@users.noreply.github.com>
2021-05-28 20:20:14 +0530

    Create Matrix Chain Multiplication(Memoization).cpp

---
+++ Dynamic Programming/Matrix Chain Multiplication/Matrix Chain Multiplication(Memoization).cpp
@@ -0,0 +1,27 @@
+class Solution{
+public:
+    int dp[101][101];
+    
+    int solve(int arr[],int i,int j)
+    {
+        int mn=INT_MAX;
+        if(i>=j)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        for(int k=i;k<=j-1;k++)
+        {
+            int tempans = solve(arr,i,k)+solve(arr,k+1,j)+(arr[i-1]*arr[k]*arr[j]);
+            if(tempans<mn)
+                mn=tempans;
+            dp[i][j]=mn;    
+        }
+        return mn;
+    }
+
+    int matrixMultiplication(int N, int arr[])
+    {
+        memset(dp,-1,sizeof(dp));
+        return solve(arr,1,N-1);
+    }
+};
