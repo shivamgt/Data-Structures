b43196bd1da84574c27627359f0be4fc6deea162
shivamgt <66057250+shivamgt@users.noreply.github.com>
2021-06-08 18:48:07 +0530

    Create Enqueue and Dequeue.cpp

---
+++ Queue/Enqueue and Dequeue.cpp
@@ -0,0 +1,25 @@
+class MyQueue {
+    int arr[100005];
+    int front;
+    int rear;
+
+public :
+    MyQueue(){front=0;rear=0;}
+    void push(int);
+    int pop();
+};
+
+//Function to push an element x in a queue.
+void MyQueue :: push(int x)
+{
+    arr[rear++]=x;
+}
+
+//Function to pop an element from queue and return that element.
+int MyQueue :: pop()
+{ 
+    if(front>=rear)
+        return -1;
+    return arr[front++];    
+}
+
