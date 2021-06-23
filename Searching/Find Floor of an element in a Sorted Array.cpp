0bb47d87f1fd77169606489b0cba9d31a3f72e06
Shivam Gupta <66057250+shivamgt@users.noreply.github.com>
2021-06-23 14:15:22 +0530

    Add files via upload

---
+++ Searching/Find Floor of an element in a Sorted Array.cpp
@@ -0,0 +1,27 @@
+int findFloor(int arr[], int N, int K) 
+{
+    int low = 0;
+    int high= N-1;
+    int res = -1;
+    
+    while(low <= high)
+    {
+        int mid =  low + (high-low)/2;
+        
+        if(arr[mid]==K)
+        {
+            res=arr[mid];
+            return res;
+        }
+        else if(arr[mid] < K)
+        {
+            res  = arr[mid];
+            low = mid +1;
+        }
+        else if(arr[mid] > K)
+        {
+            high = mid-1;
+        }
+    }
+    return res;
+}
