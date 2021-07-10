46c006a23663124eb10260d754e4ef0c192a8be6
shivamgt <66057250+shivamgt@users.noreply.github.com>
2021-07-10 19:31:48 +0530

    Create Search in a matrix.cpp

---
+++ Searching/Search in a matrix.cpp
@@ -0,0 +1,15 @@
+	int matSearch (vector <vector <int>> &mat, int N, int M, int X)
+	{
+	    int i=0;
+        int j=M-1;
+        while(i<N && j>=0)
+        {
+            if(mat[i][j]==X)
+                return 1;
+            else if(mat[i][j]>X)
+                j--;
+            else if(mat[i][j]<X)
+                i++;
+    	}
+    return 0;
+	}
