ca0afe53664ef8c011f31b0d7e59d8a1028085fb
shivamgt <66057250+shivamgt@users.noreply.github.com>
2021-08-22 21:57:42 +0530

    Create Three sum.cpp

---
+++ Array/Three sum.cpp
@@ -0,0 +1,34 @@
+ 	vector<vector<int>> threeSum(vector<int>& nums)
+    {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        
+        for(int i=0;i<n-2;i++)
+        {
+            if( i==0 || (i>0 && nums[i] != nums[i-1]) )
+            {
+                int lo=i+1;
+                int hi=nums.size()-1;
+                int sum = 0-nums[i];
+                
+                while(lo<hi)
+                {
+                    if(nums[lo]+nums[hi]==sum)
+                    {
+                        ans.push_back({nums[i],nums[lo],nums[hi]});
+                        
+                        while(lo<hi && nums[lo] == nums[lo+1]) lo++;
+                        while(lo<hi && nums[hi] == nums[hi-1]) hi--;
+                        
+                        lo++;hi--;
+                    }
+                    else if(nums[lo]+nums[hi]<sum) 
+                        lo++;
+                    else
+                        hi--;
+                }
+            }
+        }
+        return ans;
+    }
