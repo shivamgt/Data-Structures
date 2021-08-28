d683be1b96514519a181c3a1b56418bedfe45db3
shivamgt <66057250+shivamgt@users.noreply.github.com>
2021-08-28 22:02:15 +0530

    Create Rotate Image (Anti-Clockwise).cpp

---
+++ Matrix/Rotate Image (Anti-Clockwise).cpp
@@ -0,0 +1,10 @@
+	void rotateby90(vector<vector<int> >& matrix, int n) 
+    { 
+         for(int i=0;i<n;i++)
+            for(int j=0;j<i;j++)
+                swap(matrix[i][j],matrix[j][i]);
+            
+        for(int i=0;i<n/2;i++)
+            for(int j=0;j<n;j++)
+                swap(matrix[i][j],matrix[n-i-1][j]);
+    } 
