58820820816772d7573faec71b1803369ee663d0
shivamgt <66057250+shivamgt@users.noreply.github.com>
2021-08-29 23:53:34 +0530

    Create Transpose of matrix.cpp

---
+++ Matrix/Transpose of matrix.cpp
@@ -0,0 +1,11 @@
+	void transpose(vector<vector<int> >& matrix, int n)
+    { 
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+    }          
+
