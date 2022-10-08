e2783e6c4b14f5197d1c8e2fb3668c24794ee4bf
shivamgt <66057250+shivamgt@users.noreply.github.com>
2022-10-08 07:19:11 +0530

    Create 3. Insertion Sort.cpp

---
+++ Sorting/3. Insertion Sort.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int arr[], int n){
+    for(int i=1; i<n; i++){
+        int temp = arr[i];
+        int j = i-1;
+        while( j>=0 && arr[j]>temp){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = temp;
+    }
+}
+
+int main() {
+    
+    int arr[] = {5,4,3,2,1};
+    int  n= 5;
+    insertionSort(arr, 5);
+    
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+    return 0;
+}
